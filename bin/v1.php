2b7dda60eae4e86adebfcc8a953fde2b3475122c
windsor <txcses@gmail.com>
2017-02-22 14:08:35 +0800

    access control expose headers

--- bin/v1.php
+++ bin/v1.php
@@ -2,4 +2,10 @@
 
 header('content-type: application/json');
 header('access-control-allow-origin: *');
+header('access-control-expose-headers: x-power-date, content-length');
+
+header('x-power-date: ' . date('c'));
+
+header('last-modified: ' . date('c'));
+
 echo json_encode(['get' => $_GET, 'post' => $_POST]);
